e7b694c86ff77c06916e92522158e65df0f3879f
Brooklyn <58751447+bbrooklyn@users.noreply.github.com>
2022-02-23 16:27:17 +0000

    Added help

---
+++ index.py
@@ -0,0 +1,14 @@
+from stack import Stack
+
+block = Stack(10) # Allocate Memory
+
+block.push('Hello World!') # Push the data to the stack.
+
+block.pop() # Remove the last item added to the stack.
+
+#block.peek() # Returns the last item added to the stack.
+
+#block.is_full() # Returns whether the stack is full.
+
+#block.is_empty() # Returns whether the stack is empty.
+
